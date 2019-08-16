035b5b6753cb7869873cacde97baea3b294ddc6b
Daiki Ihara <sasurau4@gmail.com>
2019-08-16 17:29:14 +0900

    modify impl-future

--- impl-future/src/main.rs
+++ impl-future/src/main.rs
@@ -1,3 +1,41 @@
+extern crate futures;
+extern crate tokio;
+
+use futures::{Future, Async, Poll};
+use std::fmt;
+
+struct HelloWorld;
+struct Display<T>(T);
+
+impl Future for HelloWorld {
+    type Item = String;
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        Ok(Async::Ready("hello world".to_string()))
+    }
+}
+
+impl<T> Future for Display<T>
+where 
+    T: Future,
+    T::Item: fmt::Display,
+{
+    type Item = ();
+    type Error = T::Error;
+
+    fn poll(&mut self) -> Poll<(), T::Error> {
+        let value = match self.0.poll() {
+            Ok(Async::Ready(value)) => value,
+            Ok(Async::NotReady) => return Ok(Async::NotReady),
+            Err(err) => return Err(err),
+        };
+        println!("{}", value);
+        Ok(Async::Ready(()))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let future = Display(HelloWorld);
+    tokio::run(future);
 }
